e1a399fa65cf004af3287aa4d794a5e1849a5c08
Jung Woojin <101079472+Hyunul@users.noreply.github.com>
2023-05-01 12:55:50 +0900

    [Silver V] Title: 영화감독 숌, Time: 580 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+count = 0
+a = 0
+while True:
+    if "666" in str(a):
+        count += 1
+        
+        if count == n:
+            break
+    
+    a += 1
+    
+print(a)
