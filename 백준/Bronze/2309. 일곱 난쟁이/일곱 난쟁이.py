6c30101bafa627a2f82618a019a5009553c67b8e
Jung WooJin <101079472+Hyunul@users.noreply.github.com>
2025-02-04 08:57:38 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,15 +1,13 @@
 array = []
-for i in range(9):
+for _ in range(9):
     array.append(int(input()))
 
 array.sort()
+total = sum(array)
 
-sum_ = sum(array)
-
-# 만약 모두다 더하고 2명을 뺐을 때 그 값이 100이라면 2개를 뺀 나머지 값들 출력
 for i in range(len(array)):
-    for j in range(i + 1, len(array)):
-        if sum_ - array[i] - array[j] == 100:
+    for j in range(i+1, len(array)):
+        if total - (array[i] + array[j]) == 100:
             for k in range(len(array)):
                 if k == i or k == j:
                     pass
