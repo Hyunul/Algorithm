19e46474345bc4cba97c97d6f0dac393f5839d72
Jung WooJin <101079472+Hyunul@users.noreply.github.com>
2025-02-10 08:54:56 +0900

    [level 2] Title: 전화번호 목록, Time: 101.08 ms, Memory: 30.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,12 +1,6 @@
 def solution(phone_book):
-    hashMap = {}
-    for i in phone_book:
-        hashMap[i] = 1
-    
-    for j in phone_book:
-        arr = ""
-        for num in j:            
-            arr += num
-            if arr in hashMap and arr != j:
-                return False
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+            return False
     return True
