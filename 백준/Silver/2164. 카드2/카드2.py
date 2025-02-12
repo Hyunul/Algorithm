96a824597bc9691ed17788e21f423dd3005aa9a8
Jung WooJin <101079472+Hyunul@users.noreply.github.com>
2025-02-13 07:44:38 +0900

    [Silver IV] Title: 카드2, Time: 176 ms, Memory: 51888 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -4,11 +4,8 @@ from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
-l = deque()
-for i in range(1, n+1):
-    l.append(i)
-    
-# print(l.popleft())
+l = deque(i for i in range(1, n+1) )
+
 while len(l) > 1:
     l.popleft()
     l.append(l.popleft())
