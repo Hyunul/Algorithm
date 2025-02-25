da8f9b387be9cc20817782da248380d2ccc4cbb0
Jung WooJin <101079472+Hyunul@users.noreply.github.com>
2025-02-26 06:20:20 +0900

    [Silver IV] Title: 수 찾기, Time: 156 ms, Memory: 52272 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,9 @@
+
+n = int(input())
+n_list = set(map(int, input().split()))
+
+m = int(input())
+m_list = list(map(int, input().split()))
+
+for num in m_list:
+    print(1) if num in n_list else print(0)
