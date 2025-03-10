c7424e465c7072c693363d22cf57eda121a40f95
Jung WooJin <101079472+Hyunul@users.noreply.github.com>
2025-03-11 08:45:13 +0900

    [Silver IV] Title: 주몽, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.py
+++ 백준/Silver/1940. 주몽/주몽.py
@@ -1,23 +1,20 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
 m = int(input())
-nums = list(map(int,input().split()))
+n_list = list(map(int, input().split()))
 
-nums.sort()
-left, right = 0, len(nums) - 1
-count = 0
+n_list.sort()
+cnt = 0
+left, right = 0, len(n_list) -1
 
 while left < right:
-    sum_num = nums[left] + nums[right]
+    sum_num = n_list[left] + n_list[right]
     if sum_num < m:
         left += 1
     elif sum_num > m:
         right -= 1
     else:
-        count += 1
         left += 1
         right -= 1
+        cnt += 1
 
-print(count)
+print(cnt)
