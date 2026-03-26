cf3afe2fb56af8228120f3eb715a9baa32ea914a
Jung WooJin <jungwj1023@naver.com>
2026-03-26 15:00:52 +0900

    [Bronze I] Title: 약수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+
+if len(arr) == 1:
+    print(arr[0]**2)
+else:
+    print(arr[0]*arr[-1])
