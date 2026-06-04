2361692ce5e4a4e32e5b461c03bec185d1b84696
Jung WooJin <jungwj1023@naver.com>
2026-06-04 13:26:46 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,13 @@
+def solution(price):
+    answer = 0
+    discount = 0
+    
+    if price >= 500000:
+        discount = 0.2
+    elif price >= 300000:
+        discount = 0.1
+    elif price >= 100000:
+        discount = 0.05
+    answer = int(price - (price * discount))
+    
+    return answer
