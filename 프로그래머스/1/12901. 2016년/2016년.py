16e5f852f856e40a576ace317e7ce0a493eff027
Jung WooJin <jungwj1023@naver.com>
2026-06-09 10:34:32 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -0,0 +1,7 @@
+def solution(a, b):
+    day = [31,29,31,30,31,30,31,31,30,31,30,31]
+    week = ['FRI','SAT','SUN','MON','TUE','WED','THU']
+    
+    total = sum(day[:a-1]) + (b-1)
+    
+    return week[total % 7]
