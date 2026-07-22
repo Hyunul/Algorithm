c33fe535f07b1850d3ffca3727dca2088ac2189d
Jung WooJin <jungwj1023@naver.com>
2026-07-22 14:01:40 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 50.15 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,20 +2,22 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> map = new HashMap<>();
-        for (String part : participant) {
-            map.put(part, map.getOrDefault(part, 0) + 1);
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String name : participant) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
         }
         
-        for (String comp : completion) {
-            map.put(comp, map.get(comp) - 1);
+        for (String name : completion) {
+            map.put(name, map.get(name) - 1);
         }
         
-        for (String name : map.keySet()) {
+        for (String name : participant) {
             if (map.get(name) > 0) {
                 return name;
             }
         }
+        
         return "";
     }
 }
